f0edd02fca481e535ee93497c3da77ec700e8937
gary gritsch <garygritsch@gmail.com>
2023-12-11 10:08:39 -0800

    added navbar to top of home page

---
+++ blueprint-meadows/src/Navbar.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+
+function Navbar(){
+    return (
+        <ul>
+            <li><a href="default.asp">Home</a></li>
+            <li><a href="news.asp">News</a></li>
+            <li><a href="contact.asp">Contact</a></li>
+            <li><a href="about.asp">About</a></li>
+        </ul>
+    )
+}
+
+export default Navbar
